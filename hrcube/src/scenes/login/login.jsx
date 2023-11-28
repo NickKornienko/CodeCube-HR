8ffa2615a1521dd41b72cf9d1b2d91e33389585a
nkornien <nickkornienko@outlook.com>
2023-11-28 12:30:26 -0800

    updated redirects

--- hrcube/src/scenes/login/login.jsx
+++ hrcube/src/scenes/login/login.jsx
@@ -7,8 +7,7 @@ import KeyIcon from "@mui/icons-material/Key";
 import ViewInArRoundedIcon from "@mui/icons-material/ViewInArRounded";
 import AuthService from "../../AuthService.js";
 import { Link } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const theme = useTheme();
@@ -23,7 +22,7 @@ const Login = () => {
   const handleLogin = () => {
     AuthService.login(username, password)
       .then((response) => {
-        navigate('/dashboard');
+        navigate("/");
         console.log("Login successful", response);
       })
       .catch((error) => {
