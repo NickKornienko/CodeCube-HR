b525bd4f9413999345cd0dbc9980e21de5a1393a
nkornien <nickkornienko@outlook.com>
2023-11-28 12:16:12 -0800

    backend auth fixed

---
+++ backend/auth.controller.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User } = require("./db");
+const secrets = require("./secrets.json");
+const jwtSecret = secrets.jwtSecret;
+
+const register = async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      return res
+        .status(401)
+        .send({ message: "Incorrect username or password" });
+    }
+    const token = jwt.sign({ userId: user.id }, jwtSecret, {
+      expiresIn: "24h",
+    });
+    res.send({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+};
+
+module.exports = { register, login };
--- hrcube/src/AuthService.js
+++ hrcube/src/AuthService.js
@@ -1,51 +1,25 @@
+// AuthService.js
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/login/";
-const csrfToken = getCookie("csrftoken");
-axios.defaults.headers.common["X-CSRFToken"] = csrfToken;
+const API_URL = "http://localhost:3000/api/";
 
-function getCookie(cookieName) {
-  const name = cookieName + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(";");
-
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === " ") {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(name) === 0) {
-      return cookie.substring(name.length, cookie.length);
-    }
-  }
-  return "";
-}
+const register = (username, email, password) => {
+    return axios.post(API_URL + "register", { username, email, password });
+};
 
 const login = (username, password) => {
-  return axios.post(API_URL, { username, password }).then((response) => {
-    if (response.data.token) {
-      localStorage.setItem("user", JSON.stringify(response.data));
-    }
-    return response.data;
-  });
+    return axios.post(API_URL + "login", { username, password }).then((response) => {
+        if (response.data.token) {
+            localStorage.setItem("user", JSON.stringify(response.data));
+        }
+        return response.data;
+    });
 };
 
 const logout = () => {
-  localStorage.removeItem("user");
+    localStorage.removeItem("user");
 };
 
-const register = (username, email, password) => {
-  return axios.post(API_URL + "register/", {
-    username,
-    email,
-    password,
-  });
-};
-
-const AuthService = {
-  login,
-  logout,
-  register,
-};
+const AuthService = { login, logout, register };
 
 export default AuthService;
